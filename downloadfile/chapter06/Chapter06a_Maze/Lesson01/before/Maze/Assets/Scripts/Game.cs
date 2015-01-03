e07044c379ded4651d0b925a25b47d5c024ea763
Maruchu <maruchu_com@yahoo.co.jp>
2015-01-04 06:48:14 +0900

    CHAPTER06(迷路と対戦)のガワを追加
    CHAPTER06(迷路と対戦)のフォルダと初期のプロジェクトをコミット
    readmeを削除

---
+++ downloadfile/chapter06/Chapter06a_Maze/Lesson01/before/Maze/Assets/Scripts/Game.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+
+/*
+ *	ゲームの情報を管理するクラス
+ *	Maruchu
+ */
+public		class		Game				: MonoBehaviour {
+	
+	
+	
+	
+	private	static		bool		m_stageClearFlag		= false;	//これが true ならステージクリアとする
+	
+	/*
+	 *	ステージクリアしたら呼ばれる
+	 */
+	public	static		void		SetStageClear() {
+		//ステージクリアにする
+		m_stageClearFlag		= true;
+	}
+	
+	/*
+	 *	ステージクリアしたかどうか確認
+	 */
+	public	static		bool		IsStageCleared() {
+		return	m_stageClearFlag;
+	}
+	
+	
+	
+	
+}
---
+++ downloadfile/chapter06/Chapter06a_Maze/Lesson01/before/Maze/Assets/Scripts/Target.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/*
+ *	ターゲットクラス
+ *	Maruchu
+ *
+ *	Bulletクラスを持った GameObject が接触すると破壊される
+ *	すべてのターゲットが破壊されるとステージクリアとする
+ */
+public		class		Target				: MonoBehaviour {
+	
+	
+	
+	
+	public							GameObject	hitEffectPrefab			= null;						//ヒットエフェクトのプレハブ
+	
+	private	static					int			m_allTargetNum			= 0;						//ステージに配置されているターゲットの数
+	
+	
+	
+	/*
+	 *	起動時に呼び出される関数
+	 */
+	private		void	Awake() {
+		//ターゲットの総数を覚える
+		m_allTargetNum++;
+	}
+	
+	
+	
+	
+	/*
+	 *	Collider が何かにヒットしたら呼ばれる関数
+	 *
+	 *	自分の GameObject に Collider(IsTriggerをつける) と Rigidbody をつけると呼ばれるようになります
+	 */
+	private		void	OnTriggerEnter( Collider hitCollider) {
+		
+		//相手の GameObject を取得
+		GameObject	hitObject	= hitCollider.gameObject;
+		
+		//相手は弾？
+		if( null==hitObject.GetComponent<Bullet>()) {
+			//弾ではなかったので無視
+			return;
+		}
+		
+		
+		//破壊されたオブジェクトの処理
+		
+		//ヒットエフェクトがあるか？
+		if( null!=hitEffectPrefab) {
+			//自分と同じ位置でヒットエフェクトを出す
+			Instantiate( hitEffectPrefab, transform.position, transform.rotation);
+		}
+		
+		//ステージクリアのチェック
+		{
+			//ターゲットの総数から自分の分を削除
+			m_allTargetNum--;
+			
+			//もしターゲットの数が 0 になったらステージクリア
+			if( m_allTargetNum <= 0) {
+				
+				//ステージクリアにする
+				Game.SetStageClear();
+			}
+		}
+		
+		//この GameObject をヒエラルキーから削除
+		Destroy( gameObject);
+	}
+	
+	
+	
+	
+}
