aaf5a219d51aab60a8b393325715a8d2ea153f74
Maruchu <maruchu_com@yahoo.co.jp>
2015-01-12 04:50:29 +0900

    全Lesson分のプロジェクトファイル追加
    プロジェクト、unitypackage、コピーする元の場所の書かれたテキスト

---
+++ downloadfile/chapter06/Chapter06a_Maze/Lesson09/after/Maze/Assets/Scripts/Bullet.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/*
+ *	弾クラス
+ *	Maruchu
+ *
+ *	何でもいいので接触したらエフェクトを出して消える
+ */
+public		class		Bullet				: MonoBehaviour {
+	
+	
+	
+	private		static readonly		float		bulletMoveSpeed			= 10.0f;					//1秒間に弾が進む距離
+	
+	
+	public							GameObject	hitEffectPrefab			= null;						//ヒットエフェクトのプレハブ
+	
+	
+	
+	
+	/*
+	 *	毎フレーム呼び出される関数
+	 */
+	private		void	Update() {
+		
+		//移動
+		{
+			//1秒間の移動量
+			Vector3		vecAddPos	= (Vector3.forward		*bulletMoveSpeed);
+			/*
+				Vector3.forward は new Vector3( 0f, 0f, 1f) と同じです
+
+				他にも色々あるので↓のページを参照してみてください
+				http://docs.unity3d.com/ScriptReference/Vector3.html
+
+				そして Vector3 に transform.rotation をかけると、その方向へ曲げてくれます
+				この時、Vector3 は Z+ の方向を正面として考えます
+			 */
+			
+			//移動量、回転量には Time.deltaTime をかけて実行環境(フレーム数の差)による違いが出ないようにします
+			transform.position	+= ((transform.rotation	 	*vecAddPos)		*Time.deltaTime);
+		}
+	}
+	
+	
+	
+	/*
+	 *	Collider が何かにヒットしたら呼ばれる関数
+	 *
+	 *	自分の GameObject に Collider(IsTriggerをつける) と Rigidbody をつけると呼ばれるようになります
+	 */
+	private		void	OnTriggerEnter( Collider hitCollider) {
+		
+		//ヒットエフェクトある？
+		if( null!=hitEffectPrefab) {
+			//自分と同じ位置でヒットエフェクトを出す
+			Instantiate( hitEffectPrefab, transform.position, transform.rotation);
+		}
+		
+		//この GameObject をヒエラルキーから削除
+		Destroy( gameObject);
+	}
+	
+	
+	
+	
+}
---
+++ downloadfile/chapter06/Chapter06a_Maze/Lesson09/after/Maze/Assets/Scripts/Round.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+
+/*
+ *	汎用のくるくる回るオブジェクト用スクリプト
+ *	Maruchu
+ */
+public		class		Round				: MonoBehaviour {
+
+
+
+	private							float		rotationNow				= 0f;						//回転量のログ
+	
+	public							float		rotationAdd				= 90f;						//1秒間に回転する量
+	
+	
+	
+	/*
+	 *	毎フレーム呼び出される関数
+	 */
+	private		void	Update() {
+		
+		//回転量を加算
+		rotationNow			+= (rotationAdd	*Time.deltaTime);			//移動量、回転量には Time.deltaTime をかけて実行環境(フレーム数の差)による違いが出ないようにします
+		
+		//オイラー角で入れる
+		transform.rotation	= Quaternion.Euler( 0, rotationNow, 0);		//Y軸回転でオブジェクトの向きを横に動かせます
+	}
+	
+	
+	
+	
+}
