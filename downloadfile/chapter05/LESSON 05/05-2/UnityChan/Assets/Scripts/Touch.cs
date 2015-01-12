41fe16b2d3001bf75ea8ec2a4119c630e51444ae
AtsushiO <atsushi5703@gmail.com>
2015-01-13 01:11:33 +0900

    Chapter05 LESSON05

---
+++ downloadfile/chapter05/LESSON 05/05-2/UnityChan/Assets/Scripts/Touch.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Touch : MonoBehaviour {
+
+	public AudioClip voice_01;
+	public AudioClip voice_02;
+	
+	private Animator animator;
+	
+	private AudioSource univoice;
+	
+	// Use this for initialization
+	void Start () {
+	
+		animator	= GetComponent<Animator> ();
+		
+		univoice	= GetComponent<AudioSource> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		animator.SetBool("Touch", false);
+		
+		Ray ray;
+		RaycastHit hit;
+		
+		if( Input.GetMouseButtonDown(0) ){
+	  
+			//マウスカーソルの位置からカメラの画面を通してレイを飛ばす
+			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			
+			if (Physics.Raycast(ray,out hit, 100)) {
+				
+				Debug.Log("hit");
+				
+				
+				animator.SetBool("Touch", true);
+				
+				univoice.clip = voice_02;
+				univoice.Play();
+				
+			}
+			
+		}
+	}
+}
